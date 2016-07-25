95aacc4fa56187acaf6f1f7805c8d652fd1b5495
Jon Nicholson <jon.nicholson@zinetdatasolutions.com>
2016-07-25 19:22:37 +0100

    Added Binding repository implementation, allowing easier use rather than having ti directly use the Generic Repository.

---
+++ Code/Sif3Framework/Sif.Framework/Persistence/NHibernate/BindingRepository.cs
@@ -0,0 +1,29 @@
+﻿using Sif.Framework.Model.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sif.Framework.Persistence.NHibernate
+{
+    class BindingRepository : GenericRepository<SifObjectBinding, long>, IBindingRepository
+    {
+        public BindingRepository() : base(EnvironmentProviderSessionFactory.Instance) { }
+        
+        public string RetrieveOwnerId(Guid objectId)
+        {
+            SifObjectBinding binding = this.Retrieve(new SifObjectBinding()
+            {
+                RefId = objectId
+            }).SingleOrDefault();
+
+            if(binding == null)
+            {
+                throw new KeyNotFoundException("No owner specified for object with id " + objectId);
+            }
+
+            return binding.OwnerId;
+        }
+    }
+}
